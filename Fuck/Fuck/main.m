09b371ec88c05c101b4204eae4d059a64d81140e
lanou <lan@yanqiongnan.local>
2016-07-28 17:36:01 +0800

    fuck

---
+++ Fuck/Fuck/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fuck
+//
+//  Created by lanou on 16/7/28.
+//  Copyright © 2016年 yanqiongnan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Fuck/FuckTests/FuckTests.m
@@ -0,0 +1,39 @@
+//
+//  FuckTests.m
+//  FuckTests
+//
+//  Created by lanou on 16/7/28.
+//  Copyright © 2016年 yanqiongnan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FuckTests : XCTestCase
+
+@end
+
+@implementation FuckTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
